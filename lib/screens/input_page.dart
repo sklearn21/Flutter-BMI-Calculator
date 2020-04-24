cd2b6d61b2fa4aac1caf854ee41629f857fba3d8
santosh <sklearn21@gmail.com>
2020-04-24 08:11:07 +0530

    Added result_page.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,9 +1,13 @@
+import 'package:bmicalculator/screens/results_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'resuable_card.dart';
-import 'constants.dart';
+import '../components/icon_content.dart';
+import '../components/resuable_card.dart';
+import '../constants.dart';
+import 'results_page.dart';
+import '../components/button_bottom.dart';
+import '../components/round_icon_button.dart';
 
 enum Gender {
   male,
@@ -209,33 +213,19 @@ class _InputPageState extends State<InputPage> {
                       ],
                     ),
                   ),
-                  Container(
-                    color: kBottomContainerColor,
-                    margin: EdgeInsets.only(top: 10),
-                    width: double.infinity,
-                    height: kBottomContainerHeight,
+                  BottomButton(
+                    buttonTitle: 'CALCULATE',
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ResultsPage()));
+                    },
                   )
                 ],
               ),
             ),
           ),
         ));
-  }
-}
-
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-  final IconData icon;
-  final Function onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 0.0,
-      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
   }
 }
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,64 @@
+import 'package:bmicalculator/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../components/resuable_card.dart';
+import 'package:bmicalculator/components/button_bottom.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ResuableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.6',
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    'Your BMI result is quite low, you should eat more!',
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
